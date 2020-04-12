16fc7b203e6e13ee9be192789669e84490f1e13c
Mubeenas <mubeenas951@gmail.com>
2020-04-12 12:10:38 +0530

    first commit

--- democrossword/reports/cucumber-html-report/report.js
+++ democrossword/reports/cucumber-html-report/report.js
@@ -9,6 +9,104 @@ formatter.feature({
     }
   ]
 });
+formatter.scenarioOutline({
+  "name": "validate Login Functionality",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@TC_01_crossword"
+    }
+  ]
+});
+formatter.step({
+  "name": "I launch the browser and enter the url",
+  "keyword": "Given "
+});
+formatter.step({
+  "name": "login page is opened",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "I enter the \"\u003cusername\u003e\" and \"\u003cpassword\u003e\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "name": "I click on the signin button",
+  "keyword": "And "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ]
+    },
+    {
+      "cells": [
+        "mubeenas951@gmail.com",
+        "mubeenask"
+      ]
+    }
+  ]
+});
+formatter.scenario({
+  "name": "validate Login Functionality",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@democrossword"
+    },
+    {
+      "name": "@TC_01_crossword"
+    }
+  ]
+});
+formatter.step({
+  "name": "I launch the browser and enter the url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.StepDefinition.Login.i_launch_the_browser_and_enter_the_url()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "login page is opened",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.StepDefinition.Login.login_page_is_opened()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter the \"mubeenas951@gmail.com\" and \"mubeenask\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.StepDefinition.Login.i_enter_the__and(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on the signin button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.StepDefinition.Login.i_click_on_the_signin_button()"
+});
+formatter.result({
+  "status": "passed"
+});
 formatter.scenario({
   "name": "Login and search products",
   "description": "",
@@ -58,6 +156,112 @@ formatter.step({
 });
 formatter.match({
   "location": "com.StepDefinition.Search.search_result_are_displayed()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Login and Track Products",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@democrossword"
+    },
+    {
+      "name": "@TC_03_crossword"
+    }
+  ]
+});
+formatter.step({
+  "name": "launch the browser",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.StepDefinition.Track.launch_the_browser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the login homepage is opened",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.StepDefinition.Track.the_login_homepage_is_opened()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user track the product",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.StepDefinition.Track.the_user_track_the_product()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click on submit button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.StepDefinition.Track.click_on_submit_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "wishlist",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@democrossword"
+    },
+    {
+      "name": "@TC_04_crossword"
+    }
+  ]
+});
+formatter.step({
+  "name": "launching the browser",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.StepDefinition.Seller.launch_the_browser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "wishlist page is opened",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.StepDefinition.Seller.wishlist_page_is_opened()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "continue shopping",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.StepDefinition.Seller.continue_shopping()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "tell me available",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.StepDefinition.Seller.tell_me_available()"
 });
 formatter.result({
   "status": "passed"
--- democrossword/src/test/java/com/TestRunner/Login_Runner.java
+++ democrossword/src/test/java/com/TestRunner/Login_Runner.java
@@ -5,16 +5,19 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
-
+import com.cucumber.listener.Reporter;
+//import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features ="C:\\Users\\DELL\\eclipse-workspace\\democrossword\\src\\test\\resources\\features\\testcase.feature",
+				 
 	    glue = {"com.StepDefinition"},
 		plugin = {"pretty", "html:reports/cucumber-html-report",
-				},
-	  tags = {"@TC_02_crossword",},
+				"junit:target/cucumber-junit-report",
+        "com.cucumber.listener.ExtentCucumberFormatter:target/Extentreports/Extentreport.html"},
+				
+	  //tags = {"@TC_02_crossword",},
 		monochrome = true,
 	
 		strict = true
